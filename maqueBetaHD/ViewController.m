d93cafe773b45dc7361c6d7ec81c5abb504efa35
FredHsu <FredHsu0822@hotmail.com>
2016-11-07 15:26:22 +0800

    Initial Commit

---
+++ maqueBetaHD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  maqueBetaHD
+//
+//  Created by 许文锋 on 16/11/7.
+//  Copyright © 2016年 fred. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ maqueBetaHD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  maqueBetaHD
+//
+//  Created by 许文锋 on 16/11/7.
+//  Copyright © 2016年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
